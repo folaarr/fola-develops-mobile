4cbfd35509ea080f93391c3af39a68a008075b5c
Folajimi Abolade <folajimiabolade@gmail.com>
2026-02-23 12:59:06 +0100

    start

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CategoriesScreen from './screens/CategoriesScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style="light" />
+      <Stack.Navigator
+        screenOptions={{
+          headerTitleAlign: 'center', 
+          headerStyle: {backgroundColor: "#2b3035"},
+          headerTintColor: "#E7F2E4",
+          contentStyle: {
+            backgroundColor: "#2b3035"
+          }
+        }}
+      >
+        <Stack.Screen 
+          name='CategoriesScreen'
+          component={CategoriesScreen} 
+          options={{
+            title: 'Categories Screen'
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,5 @@
+import { StyleSheet } from "react-native";
+
+export default function CategoriesScreen() {};
+
+const styles = StyleSheet.create({});
