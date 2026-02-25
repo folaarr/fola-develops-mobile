9d4fd0f615b3a472a6a24ba93d283365951e8032
Folajimi Abolade <folajimiabolade@gmail.com>
2026-02-25 14:26:35 +0100

    add categories

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -6,7 +6,7 @@ export default function CategoriesScreen() {
     return (
         <ImageBackground style={styles.imageBackground} source={{uri: 'https://res.cloudinary.com/foladevelops/image/upload/v1772024627/using-gadgets_jgikxb.jpg'}}>
             <OutlinedButton name='E-store' icon={{name: 'cart-variant', size: 16, color: Colors.accent400}} />
-            <OutlinedButton name='AI app' icon={{name: 'robot-excited-outline', size: 16, color: Colors.accent400}}  />
+            <OutlinedButton name='AI app' icon={{name: 'robot-outline', size: 16, color: Colors.accent400}}  />
         </ImageBackground>
     );
 };
