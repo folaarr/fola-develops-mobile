1e3928ccc83a472a69839d54bfc77264cb277d78
Folajimi Abolade <folajimiabolade@gmail.com>
2026-02-25 14:24:32 +0100

    add categories

---
+++ components/OutlinedButton.js
@@ -0,0 +1,38 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Colors } from "../constants/colors";
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
+export default function OutlinedButton({name, onPress, icon}) {
+    return(
+        <Pressable style={({pressed}) => [styles.pressable, pressed && styles.pressed]} onPress={onPress} >
+            <View style={styles.view}>
+                <MaterialCommunityIcons {...icon} />
+                <Text style={styles.text}>{name}</Text>
+            </View>
+        </Pressable>
+    );
+};
+
+const styles = StyleSheet.create({
+    pressable: {
+        maxWidth: 125, 
+        marginTop: 8
+    },
+    pressed: {
+        opacity: 0.35
+    },
+    view: {
+        borderWidth: 0.3,
+        borderColor: Colors.accent400,
+        alignItems: 'center', 
+        paddingHorizontal: 8,
+        paddingVertical: 10,
+        borderRadius: 5, 
+        flexDirection: 'row', 
+        gap: 6
+    }, 
+    text: {
+        color: Colors.accent400,
+        fontSize: 16
+    }
+});
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,5 +1,22 @@
-import { StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet, View } from "react-native";
+import OutlinedButton from "../components/OutlinedButton";
+import { Colors } from "../constants/colors";
 
-export default function CategoriesScreen() {};
+export default function CategoriesScreen() {
+    return (
+        <ImageBackground style={styles.imageBackground} source={{uri: 'https://res.cloudinary.com/foladevelops/image/upload/v1772024627/using-gadgets_jgikxb.jpg'}}>
+            <OutlinedButton name='E-store' icon={{name: 'cart-variant', size: 16, color: Colors.accent400}} />
+            <OutlinedButton name='AI app' icon={{name: 'robot-excited-outline', size: 16, color: Colors.accent400}}  />
+        </ImageBackground>
+    );
+};
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    imageBackground: {
+        flex: 1,
+        flexDirection: 'row',
+        gap: 25,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
