e127fb2d162261ba129ee743452d3a075ab77987
Folajimi Abolade <folajimiabolade@gmail.com>
2026-02-25 13:20:29 +0100

    add keyboard avoiding view for log in text input pane

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,16 @@
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View, KeyboardAvoidingView, ScrollView, Dimensions } from "react-native";
 import LoginForm from "../components/LoginForm";
 
 export default function LoginScreen() {
+    const height = Dimensions.get('window').height;
+
     return (
         <ImageBackground source={{uri: 'https://res.cloudinary.com/foladevelops/image/upload/v1771935222/formal-meeting_hyzuwp.jpg'}} style={styles.imageBackground}>
-            <View style={styles.formCard}>
-                <LoginForm />
-            </View>
+            <ScrollView style={styles.scroll} contentContainerStyle={styles.scrollViewContainer}>
+                <KeyboardAvoidingView style={styles.avoiding} behavior='position'>
+                      <LoginForm />
+                </KeyboardAvoidingView>
+            </ScrollView>
         </ImageBackground>
     );
 };
@@ -15,9 +19,20 @@ const styles = StyleSheet.create({
     imageBackground: {
         flex: 1
     }, 
-    formCard: {
+    scroll: {
+        flex: 1,
+    },
+    scrollViewContainer: {
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    },
+    avoiding: {
         flex: 1,
         justifyContent: 'center', 
         alignItems: 'center'
+    },
+    formCard: {
+        flex: 1,
     }
 });
